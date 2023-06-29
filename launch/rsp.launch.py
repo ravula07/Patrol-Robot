22d342b5ef3db514dc328bd679b37f441d10abe2
Ravula Sai <96985470+ravula07@users.noreply.github.com>
2023-06-29 10:34:21 +0530

    Update rsp.launch.py

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,21 +1,16 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-
 import xacro
-
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('my_rosject'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
@@ -29,8 +24,6 @@ def generate_launch_description():
         parameters=[params]
     )
 
-
-    # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
